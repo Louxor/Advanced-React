d98d8c4e5e3e168a707bbdc502fffee31e759dc6
Louxor <lisa.dutour@gmail.com>
2020-10-13 17:28:36 +0200

    feat(graphql): get server running

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,14 @@
-// let's go!
+require('dotenv').config({path: 'variables.env'});
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+server.start({
+    cors: {
+        credential: true,
+        origin: process.env.FRONTEND_URL,
+    },
+}, deets => {
+    console.log(`server started on http://localhost:${deets.port}`);
+})
