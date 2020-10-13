3d071d161fea6fbaa4e8f23799ab8e3c3cbbcd96
Louxor <lisa.dutour@gmail.com>
2020-10-13 21:06:08 +0200

    feat(db): create item

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,8 +1,12 @@
+// const { forwardTo } = require('prisma-binding');
+
 const Query = {
-    dogs(parent, args, ctx, info) {
-        global.dogs = global.dogs || [];
-        return global.dogs
+    async item(parent, args, ctx, info) {
+        const items = await ctx.db.query.items();
+        return items
     }
+
+    // items: forwardTo('db'),
 };
 
 module.exports = Query;
