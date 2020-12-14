3a36ccfc32e973bc20489d256ef1d116d3b3f95c
Louxor <lisa.dutour@gmail.com>
2020-12-14 17:19:08 +0100

    feat(images): work on upload

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -29,16 +29,35 @@ const CREATE_ITEM_MUTATION = gql`
 
 class CreateItem extends Component {
     state = {
-        title: '',
-        description: '',
-        image: '',
-        largeImage: '',
-        price: 0
+        title: 'Yes',
+        description: 'we',
+        image: 'can.jpg',
+        largeImage: 'large-can.jpg',
+        price: 1000
     };
     handleChange = e => {
         const { name, type, value } = e.target;
         const val = type === 'number' ? parseFloat(value) : value;
         this.setState({ [name] : val })
+    };
+    uploadFile = async (e) => {
+        console.log('uploading file...');
+        const files = e.target.files;
+        const data = new FormData();
+        data.append('file', files[0]);
+        data.append('upload_preset', 'SICKFITS');
+
+        const res = await fetch('https://api.cloudinary.com/v1_1/dzoh2il73/image/upload', {
+            method: 'POST',
+            body: data
+        });
+
+        const file = await res.json();
+        console.log(file);
+        this.setState({
+            image: file.secure_url,
+            largeImage: file.eager[0].secure_url
+        });
     };
     render() {
         return (
@@ -58,6 +77,20 @@ class CreateItem extends Component {
                     }}>
                         <Error error={error} />
                         <fieldset disabled={loading} aria-busy={loading}>
+                            <label htmlFor="file">
+                                Image
+                                <input 
+                                    type="file" 
+                                    id="file" 
+                                    name="file" 
+                                    placeholder="Image" 
+                                    required 
+                                    value={this.state.image}
+                                    onChange={this.uploadFile}
+                                />
+                            </label>
+
+
                             <label htmlFor="title">
                                 Title
                                 <input 
