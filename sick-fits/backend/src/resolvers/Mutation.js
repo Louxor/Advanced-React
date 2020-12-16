fb4d4d7ef31789361321662a671894ffa311a4da
Louxor <lisa.dutour@gmail.com>
2020-12-16 15:42:16 +0100

    feat(back): delete item

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,38 +1,49 @@
 const Mutations = {
-    async createItem(parent, args, ctx, info) {
-      // TODO: Check if they are logged in
-  
-      const item = await ctx.db.mutation.createItem(
-        {
-          data: {
-            ...args,
-          },
-        },
-        info
-      );
-  
-      console.log(item);
-  
-      return item;
-    },
+  //definitions: https://www.apollographql.com/docs/apollo-server/data/resolvers/#resolver-arguments
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
 
-    updateItem(parent, args, ctx, info) {
-      // first take a copy of the updates
-      const updates = { ...args };
-      // remove the ID from the updates
-      delete updates.id;
-      // run the update method
-      return ctx.db.mutation.updateItem(
-        {
-          data: updates,
-          where: {
-            id: args.id,
-          },
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
         },
-        info
-      );
-    },
+      },
+      info
+    );
 
-  };
-  
-  module.exports = Mutations;
+    console.log(item);
+
+    return item;
+  },
+
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args };
+    // remove the ID from the updates
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id,
+        },
+      },
+      info
+    );
+  },
+
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id:args.id };
+    // find the item
+    const item = await ctx.db.query.item({ where }, `{ id title }`);
+    // check if own item (premission)
+    // TO DO
+    // delete
+    return ctx.db.mutation.deleteItem( { where }, info );
+  }
+
+};
+
+module.exports = Mutations;
