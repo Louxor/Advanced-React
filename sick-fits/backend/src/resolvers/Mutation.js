52ba107d1f2de8f516b2254ce458ab7feafa455e
Louxor <lisa.dutour@gmail.com>
2021-01-05 11:30:25 +0100

    feat(login): signup mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
   //definitions: https://www.apollographql.com/docs/apollo-server/data/resolvers/#resolver-arguments
   async createItem(parent, args, ctx, info) {
@@ -12,7 +15,7 @@ const Mutations = {
       info
     );
 
-    console.log(item);
+    // console.log(item);
 
     return item;
   },
@@ -42,7 +45,34 @@ const Mutations = {
     // TO DO
     // delete
     return ctx.db.mutation.deleteItem( { where }, info );
-  }
+  },
+
+  async signup(parent, args, ctx, info) {
+    // lowercase their email
+    args.email = args.email.toLowerCase();
+    // hash their password
+    const password = await bcrypt.hash(args.password, 10);
+    // create the user in the database
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] },
+        },
+      },
+      info
+    );
+    // create the JWT token for them
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // We set the jwt as a cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    });
+    // Finalllllly we return the user to the browser
+    return user;
+  },
 
 };
 
